9205b765c4da3116b0e393fe3a8af7f54c80d718
Christian-Skilar <57668355+Christian-Skilar@users.noreply.github.com>
2020-01-08 15:50:02 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+//----- Question 1 -----
+var petArray = [ "cat", "cow", "dog" ];
+console.log(petArray);
+
+//----- Question 2 -----
+console.log("cow");
+
+//----- Question 3 -----
+petArray.push("sheep");
+
+//----- Question 4 -----
+console.log(petArray.length);
+
+//----- Question 5 -----
+var catObject = {
+	name: "garfield",
+	color: "brown",
+	age: 5
+};
+
+//----- Question 6 -----
+console.log(catObject.age);
+
+//----- Question 7 -----
+var catArray = [ "garfield", "brown", "5" ];
+
+//----- Question 8 -----
+for (var i = 0; i < catArray.length; i++) {
+	console.log(catArray[i]);
+}
+
+//----- Question 9 -----
+function logToConsole(animal) {
+	console.log(animal);
+}
+
+//----- Question 10 -----
+logToConsole("donkey");
